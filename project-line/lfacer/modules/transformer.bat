bc672d7f76ce76d1b3bdf33e3d3e8e931a8e9fce
IloveEi <118670244+IloveEi@users.noreply.github.com>
2023-05-03 15:50:32 +0800

    Delete project-line directory

--- project-line/lfacer/modules/transformer.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\Administrator\Desktop\project-line\lfacer\modules
-set /p name=enter your transformed name:
-pyuic5 -o %name%.py untitled.ui
-D:/python389/python.exe c:/Users/Administrator/Desktop/project-line/lfacer/LMC.py
-pause
